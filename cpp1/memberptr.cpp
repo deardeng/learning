d145c575ad0c2dcd350b321cfcdf449bf56d956f
deardeng <565620795@qq.com>
2014-05-20 23:22:33 +0800

    deardeng's code 2014.05.20

--- cpp1/memberptr.cpp
+++ cpp1/memberptr.cpp
@@ -20,8 +20,11 @@ int main()
 	date a[5]={{2010,8,16},{2010,10,1},{2008,8,8},{2012,12,20},{1999,12,30}};
 	date d={1997,7,7};
 	cout << "&d=" << &d << endl;
+        cout << "--------------------------" << endl;
 	cout << "&d.year=" << &d.year << ", &d.month=" << &d.month << ", &d.day=" << &d.day << endl;
-	cout << &date::year << &date::month << &date::day << endl;
+        cout << "--------------------------" << endl;
+	cout << &date::year << &date::month << &date::day << endl;//类里相对地址
+        cout << "--------------------------" << endl;
 //	cout << &main << &f << endl;
 	union{
 		int n;
@@ -30,9 +33,11 @@ int main()
 	};
         p=main;
         cout << "main=" << reinterpret_cast<void *>(n) << endl;
+        cout << "--------------------------" << endl;
 	mp = &date::day;
 	cout << "n=" << n << endl;
 	cout << d.*mp << endl;
+        cout << "--------------------------" << endl;
 	mp = &date::year;
 	cout << d.*mp << endl;
 	showmember(a, 5, &date::month);
