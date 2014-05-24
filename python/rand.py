fdb53db71bb678d5a8e0577d77f78c5621ab350f
deardeng <565620795@qq.com>
2014-05-24 17:05:46 +0800

    deardeng's code 2014.05.24

--- python/rand.py
+++ python/rand.py
@@ -3,6 +3,6 @@ i=0
 import random
 while i<100:
   a=random.randint(1,1000);
-  f.write(str(a)+'\n')
+  f.write(str(a)+' ')
   i=i+1
 f.close()
--- python/read.py
+++ python/read.py
@@ -3,5 +3,10 @@ f=open('random.txt','r')
 s=f.read()
 s1 = re.split(' ',s)
 for i in s1:
-  if not i.isalnum() and int(i)%7==0:
-    print i
+###  if i.isdigit():
+###    a = int(i)
+###  if a%7==0:
+###    print a
+  if i.isdigit() :
+    if int(i)%7 == 0:
+      print i
