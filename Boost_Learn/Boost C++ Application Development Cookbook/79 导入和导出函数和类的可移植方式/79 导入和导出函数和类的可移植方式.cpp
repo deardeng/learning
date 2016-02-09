5ba945c30b18caaa1097196b85a96cb91bc5aea1
deardeng <565620795@qq.com>
2016-02-10 00:21:43 +0800

    2016.2.9

---
+++ Boost_Learn/Boost C++ Application Development Cookbook/79 导入和导出函数和类的可移植方式/79 导入和导出函数和类的可移植方式.cpp
@@ -0,0 +1,20 @@
+#include "my_library.hpp"
+#include <cassert>
+
+int main()
+{
+	assert(foo() == 0);
+	bar b;
+	try
+	{
+		b.meow();
+		assert(false);
+	}
+	catch (const bar_exception&)
+	{
+
+	}
+
+	system("pause");
+	return 0;
+}
